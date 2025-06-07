9a6c8c4d4f4f75c92a2a062add2c4b702c57f028
Efe <smiththomas30@gmail.com>
2025-06-07 10:34:47 +0200

    Implemented frontend changes for signup/login/google auth/forgot password

--- src/shared/components/auth/login/index.jsx
+++ src/shared/components/auth/login/index.jsx
@@ -18,6 +18,7 @@ import PrimaryButton from "@/shared/components/buttons/primaryButton";
 
 // Redux
 import { signInUser } from "@/shared/redux/slices/user";
+import GoogleAuthButton from "@/components/buttons/GoogleAuthButton";
 
 export default function Loginform() {
   const { isLoading, onSubmitFunction } = useSubmitFunction();
@@ -36,11 +37,13 @@ export default function Loginform() {
       data,
     });
   };
+
+
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Welcome Back 👋</h1>
       <span className={styles.subTitle}>
-        To keep connected with us please login with your personal info
+        Stay connected with us please login with your personal info
       </span>
       <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
         <ControlledTextInput
@@ -62,12 +65,18 @@ export default function Loginform() {
         />
 
         <div className={styles.forgetPassword}>
-          <Link href={"/auth/forgotpassword"}>Forgot Password?</Link>
+          <Link href={"/auth/forgotpassword"}>
+            Forgot Password?
+          </Link>
         </div>
 
         <div className={styles.submitButtonWrapper}>
           <PrimaryButton buttonText={"Login"} loading={isLoading} />
         </div>
+
+        <div className={styles.divider}>or</div>
+
+        <GoogleAuthButton mode="login" />
 
         <div className={styles.dontHaveAccount}>
           <span>
