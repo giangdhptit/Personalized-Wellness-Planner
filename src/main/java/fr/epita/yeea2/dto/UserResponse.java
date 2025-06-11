bf0617f57b54f79bdc3441cc2da65f07529176f1
giangdh <gianglibra1710@gmail.com>
2025-06-12 01:51:07 +0200

    firstName and lastName added 2

--- src/main/java/fr/epita/yeea2/dto/UserResponse.java
+++ src/main/java/fr/epita/yeea2/dto/UserResponse.java
@@ -13,11 +13,15 @@ public class UserResponse {
     private String email;
     private List<String> roles;
     private String accessToken;
+    private String lastName;
+    private String firstName;
 
     public static UserResponse from(AppUser user, String token) {
         return UserResponse.builder()
                 .id(user.getId())
                 .email(user.getEmail())
+                .firstName(user.getFirstName())
+                .lastName(user.getLastName())
                 .roles(user.getRoles())
                 .accessToken(token)
                 .build();
